b67010598e4cad5f0647b9be08a8bbec4a65e29c
Corseque <corseque@ya.ru>
2023-05-05 15:08:13 +0300

    lesson 7: token with sessions

--- src/main/java/ru/api/security/api/AuthenticationGateway.java
+++ src/main/java/ru/api/security/api/AuthenticationGateway.java
@@ -1,14 +1,12 @@
 package ru.api.security.api;
 
 import org.springframework.http.ResponseEntity;
-import org.mapstruct.Context;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import ru.api.security.AuthenticationUserDto;
 
-import javax.servlet.http.HttpServletRequest;
 
 public interface AuthenticationGateway {
     @PostMapping("/login")
-    ResponseEntity<?> login(@RequestBody AuthenticationUserDto authenticationUserDto, @Context HttpServletRequest request);
+    ResponseEntity<?> login(@RequestBody AuthenticationUserDto authenticationUserDto);
 }
